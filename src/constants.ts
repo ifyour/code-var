ccafd1995dc221c6a7b284b9c2503d7c3ca05f19
ifyour <ifyour@outlook.com>
2023-03-18 23:12:18 +0800

    feat: add cases type

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,4 @@
 /**
  * Turn on the type of query needed on demand
  */
-export const CASES = [
-  "camelCase",
-  "PascalCase",
-  "CONSTANT_CASE",
-  //  "param-case",
-  // "snake_case",
-] as const;
+export const CASES = ["camelCase", "PascalCase", "CONSTANT_CASE", "param-case", "snake_case"] as const;
